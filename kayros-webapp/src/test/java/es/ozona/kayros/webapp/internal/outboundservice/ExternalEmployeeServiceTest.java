97a92ce58734fac9834ee063351bee48226262ca
Pablo Liste Cancela <pablo.liste@ozonacloud.com>
2020-10-22 14:41:11 +0200

    KAYROS-93

--- kayros-webapp/src/test/java/es/ozona/kayros/webapp/internal/outboundservice/ExternalEmployeeServiceTest.java
+++ kayros-webapp/src/test/java/es/ozona/kayros/webapp/internal/outboundservice/ExternalEmployeeServiceTest.java
@@ -2,6 +2,10 @@ package es.ozona.kayros.webapp.internal.outboundservice;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import es.ozona.data.inquire.model.paging.PageResult;
+import es.ozona.kayros.webapp.domain.model.Employee;
 import es.ozona.kayros.webapp.infrastructure.feingclients.EmployeeService;
 import es.ozona.kayros.webapp.shareddomain.model.EmployeeResource;
 
@@ -21,10 +26,82 @@ public class ExternalEmployeeServiceTest {
 	@MockBean
 	EmployeeService employeeService;
 
+	private String employeeId;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private boolean telecommuting;
+	private String workplace;
+	private String username;
+	private String invalidUsername;
+
+	private Employee employee;
+	private EmployeeResource employeeResource;
+	private PageResult<EmployeeResource> pageResult;
+	private PageResult<EmployeeResource> emptyPageResult;
+
+	@BeforeEach
+	public void init() {
+
+		employeeId = "e7c1c31b-c936-4a8b-ad9e-46a4a86381cd";
+		email = "email";
+		firstName = "firstname";
+		lastName = "lastname";
+		telecommuting = false;
+		workplace = "workplace";
+		username = "jeijo";
+		invalidUsername = "qwertyuioplkjhgfadszcxvbnnm";
+
+		employee = new Employee(employeeId, username, email, firstName, lastName, telecommuting, workplace);
+		employeeResource = new EmployeeResource(employeeId, username, email, firstName, lastName, telecommuting, workplace);
+		pageResult = new PageResult<EmployeeResource>();
+		emptyPageResult = new PageResult<EmployeeResource>();
+
+		ArrayList<EmployeeResource> arrayList = new ArrayList<EmployeeResource>();
+		arrayList.add(employeeResource);
+
+		pageResult.setItems(arrayList);
+
+	}
+
 	@Test
 	public void givenExistingEmpleeUsername_whenCallFindEmployeeByUsername_thenReturnsEmployee() {
-		Mockito.when(employeeService.search("username:jeijo", "+username", 1, 1)).thenReturn(new PageResult<EmployeeResource>());
-		assertThat(externalEmployeeService.findEmployeeByUsername("jeijo")).isNotNull();
+
+		Mockito.when(employeeService.search("username:%s".formatted(username), "+username", 1, 1)).thenReturn(pageResult);
+		assertThat(externalEmployeeService.findEmployeeByUsername(username)).isNotEqualTo(Optional.empty());
+
+	}
+
+	@Test
+	public void givenExistingEmpleeUsername_whenCallFindEmployeesLikeUsername_thenReturnsEmployees() {
+
+		Mockito.when(employeeService.search("username:%s*".formatted(username), "+username", 1, 15)).thenReturn(pageResult);
+		assertThat(externalEmployeeService.findEmployeesLikeUsername(username).size() > 0).isTrue();
+
+	}
+
+	@Test
+	public void givenInvalidEmpleeUsername_whenCallFindEmployeeByUsername_thenReturnsNoEmployee() {
+
+		Mockito.when(employeeService.search("username:%s".formatted(invalidUsername), "+username", 1, 1)).thenReturn(emptyPageResult);
+		assertThat(externalEmployeeService.findEmployeeByUsername(invalidUsername)).isEqualTo(Optional.empty());
+
+	}
+
+	@Test
+	public void givenInvalidEmpleeUsername_whenCallFindEmployeesLikeUsername_thenReturns0Employees() {
+
+		Mockito.when(employeeService.search("username:%s*".formatted(invalidUsername), "+username", 1, 15)).thenReturn(emptyPageResult);
+		assertThat(externalEmployeeService.findEmployeesLikeUsername(invalidUsername).size() == 0).isTrue();
+
+	}
+
+	@Test
+	public void givenEmployee_whenCallModifyEmployee_thenReturnEmployeeObject() {
+
+		Mockito.when(employeeService.modify(employeeResource, employeeId)).thenReturn(employeeResource);
+		assertThat(externalEmployeeService.modifyEmployee(employee) instanceof Employee).isTrue();
+
 	}
 
 }
