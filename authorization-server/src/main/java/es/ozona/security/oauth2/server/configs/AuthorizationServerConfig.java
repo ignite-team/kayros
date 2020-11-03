cc98e62a6ac261e873cf740d6ae3282ca864f2da
vinrhael <vinrhael@gmail.com>
2020-11-03 13:11:12 +0100

    Parametrización entornos.

--- authorization-server/src/main/java/es/ozona/security/oauth2/server/configs/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/es/ozona/security/oauth2/server/configs/AuthorizationServerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
