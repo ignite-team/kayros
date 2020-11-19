2f75b136852d57c8c98f8c14886b21ed5fcf8b1b
Pablo Liste Cancela <pablo.liste@ozonacloud.com>
2020-11-19 12:51:19 +0100

    HTTPS Authorization-server

--- authorization-server/src/main/java/es/ozona/security/oauth2/server/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/es/ozona/security/oauth2/server/AuthorizationServerApplication.java
@@ -1,9 +1,16 @@
 package es.ozona.security.oauth2.server;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableEurekaClient
@@ -12,6 +19,31 @@ public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
+	}
+
+	private Connector redirectConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(9100);
+		connector.setRedirectPort(9443);
+		return connector;
 	}
 
 }
