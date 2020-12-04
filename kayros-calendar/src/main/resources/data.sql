04e4eb82229146f5dd1a4f37b9d72d7343e2c7fb
Pablo Liste Cancela <pablo.liste@ozonacloud.com>
2020-12-04 14:04:03 +0100

    Fixed data type issues

--- kayros-calendar/src/main/resources/data.sql
+++ kayros-calendar/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into calendars (calendar_id,description, marked_as_default, title, year) values('2B477572BD4A4C28A50464C9486492CC', 'Calendario general para el año 2020', true, 'Calendario laboral 2020', 2020 );
+insert into calendars (calendar_id,description, marked_as_default, title, year) values('2b477572bd4a4c28a50464c9486492cc', 'Calendario general para el año 2020', true, 'Calendario laboral 2020', 2020 );
 
-insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2B477572BD4A4C28A50464C9486492CC', '2020-06-30', '2020-06-30', '2020-01-01', '2B477572BD4A4C28A50464C948649211');
-insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2B477572BD4A4C28A50464C9486492CC', '2020-08-31', '2020-08-31', '2020-07-01', '2B477572BD4A4C28A50464C9486495AA');
-insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2B477572BD4A4C28A50464C9486492CC', '2020-12-31', '2020-12-31', '2020-09-01', '2B477572BD4A4C28A50464C9486495CC');
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2020-06-30', '2020-06-30', '2020-01-01', '2b477572bd4a4c28a50464c948649211');
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2020-08-31', '2020-08-31', '2020-07-01', '2b477572bd4a4c28a50464c9486495aa');
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2020-12-31', '2020-12-31', '2020-09-01', '2b477572bd4a4c28a50464c9486495cc');
 
 insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',0,'0:30','08:00','17:00', 1);
 insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',1,'0:30','08:00','17:00', 1);
