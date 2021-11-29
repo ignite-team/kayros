2eba609c78f15a23d948104ea779d23b5b0a93df
vinrhael <vinrhael@gmail.com>
2021-11-29 11:43:24 +0100

    Exportación XLSX

--- kayros-webapp/src/main/java/es/ozona/kayros/webapp/utils/ExportUtils.java
+++ kayros-webapp/src/main/java/es/ozona/kayros/webapp/utils/ExportUtils.java
@@ -2,7 +2,6 @@ package es.ozona.kayros.webapp.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -20,10 +19,12 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zkoss.util.resource.Labels;
 
 public class ExportUtils {
-
+	private static final Logger LOG = LoggerFactory.getLogger(ExportUtils.class);
 	private static final String WORKING_TIMEPERIODS_TEXT = Labels.getLabel("timesheet.workingTimePeriods");
 	private static final String YES_TEST = Labels.getLabel("general.yes");
 	private static final String NO_TEXT = Labels.getLabel("general.no");
@@ -143,22 +144,7 @@ public class ExportUtils {
 			
 		}
 
-		/*
-		 * Object[][] datatypes = { {"Datatype", "Type", "Size(in bytes)"}, {"int",
-		 * "Primitive", 2}, {"float", "Primitive", 4}, {"double", "Primitive", 8},
-		 * {"char", "Primitive", 1}, {"String", "Non-Primitive", "No fixed size"} };
-		 * 
-		 * int rowNum = 0; System.out.println("Creating excel");
-		 * 
-		 * for (Object[] datatype : datatypes) { Row row = sheet.createRow(rowNum++);
-		 * int colNum = 0; for (Object field : datatype) { Cell cell =
-		 * row.createCell(colNum++); if (field instanceof String) {
-		 * cell.setCellValue((String) field); } else if (field instanceof Integer) {
-		 * cell.setCellValue((Integer) field); } } }
-		 */
-		try
-
-		{
+		try {
 
 			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 			workbook.write(buffer);
@@ -168,109 +154,13 @@ public class ExportUtils {
 			InputStream inputStream = new ByteArrayInputStream(bytes);
 
 			return inputStream;
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			
 		} catch (IOException e) {
-			e.printStackTrace();
+			LOG.error("No se ha podido generar el fichero de exportación a Excel.", e);
 		}
-
-		System.out.println("Done");
 
 		return null;
 
-		/*
-		 * Workbook workbook = new XSSFWorkbook();
-		 * 
-		 * try {
-		 * 
-		 * 
-		 * 
-		 * Sheet sheet = workbook.createSheet(WORKING_TIMEPERIODS_TEXT);
-		 * 
-		 * Row header = sheet.createRow(0);
-		 * 
-		 * for (int x = 0; x < headers.size(); x++) {
-		 * 
-		 * sheet.setColumnWidth(x, rows.size()); Cell cell = header.createCell(x);
-		 * cell.setCellValue(headers.get(x));
-		 * 
-		 * }
-		 * 
-		 * for (int y = 0; y < rows.size(); y++) {
-		 * 
-		 * Row row = sheet.createRow(y + 1);
-		 * 
-		 * for (int z = 0; z < rows.get(y).size(); z++) {
-		 * 
-		 * Cell cell = row.createCell(z);
-		 * 
-		 * if (rows.get(y).get(z) instanceof Boolean) {
-		 * 
-		 * if ((Boolean) rows.get(y).get(z) == true) {
-		 * 
-		 * cell.setCellValue(YES_TEST);
-		 * 
-		 * } else {
-		 * 
-		 * cell.setCellValue(NO_TEXT);
-		 * 
-		 * }
-		 * 
-		 * } else if (rows.get(y).get(z) instanceof Date) {
-		 * 
-		 * cell.setCellValue(fullFormater.format(rows.get(y).get(z)));
-		 * 
-		 * } else if (rows.get(y).get(z) instanceof LocalDate) {
-		 * 
-		 * cell.setCellValue(formater.format(Date.from(((LocalDate)
-		 * rows.get(y).get(z)).atStartOfDay(ZoneId.systemDefault()).toInstant())));
-		 * 
-		 * } else {
-		 * 
-		 * cell.setCellValue(rows.get(y).get(z) == null ? null :
-		 * rows.get(y).get(z).toString());
-		 * 
-		 * }
-		 * 
-		 * }
-		 * 
-		 * }
-		 * 
-		 * ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-		 * 
-		 * workbook.write(new FileOutputStream(new
-		 * File("c:\\proxectos\\registro.xls"))); workbook.close();
-		 * 
-		 * 
-		 * 
-		 * buffer.flush(); buffer.close();
-		 * 
-		 * byte[] bytes = buffer.toByteArray(); InputStream inputStream = new
-		 * ByteArrayInputStream(bytes);
-		 * 
-		 * buffer.close();
-		 * 
-		 * return inputStream;
-		 * 
-		 * } catch (IOException e) {
-		 * 
-		 * return null;
-		 * 
-		 * } finally {
-		 * 
-		 * try {
-		 * 
-		 * workbook.close();
-		 * 
-		 * } catch (IOException e) {
-		 * 
-		 * return null;
-		 * 
-		 * }
-		 * 
-		 * }
-		 */
 	}
 
 }
