32ffd441d65992bc5cb5c4fbfa4a5f348ab72a75
vinrhael <vinrhael@gmail.com>
2021-11-29 11:38:27 +0100

    corrección export XLSX

---
+++ kayros-calendar/src/main/resources/data-h2.sql
@@ -0,0 +1,23 @@
+insert into calendars (calendar_id,description, marked_as_default, title, year) values('2b477572bd4a4c28a50464c9486492cc', 'Calendario general para el año 2021', true, 'Calendario laboral 2021', 2021 );
+
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2021-06-30', '2021-06-30', '2021-01-01', '2b477572bd4a4c28a50464c948649211');
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2021-08-31', '2021-08-31', '2021-07-01', '2b477572bd4a4c28a50464c9486495aa');
+insert into shiftplans (calendar_id, efective_end_date, end_date, start_date, shiftplan_id) values ('2b477572bd4a4c28a50464c9486492cc', '2021-12-31', '2021-12-31', '2021-09-01', '2b477572bd4a4c28a50464c9486495cc');
+
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',0,'0:30','08:00','17:00', 1);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',1,'0:30','08:00','17:00', 1);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',2,'0:30','08:00','17:00', 1);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',3,'0:30','08:00','17:00', 1);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,4,'00:20','08:00','20:00', 1);
+
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,0,'00:20','08:00','17:00', 2);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,1,'00:20','08:00','17:00', 2);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,2,'00:20','08:00','17:00', 2);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,3,'00:20','08:00','17:00', 2);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,4,'00:20','08:00','20:00', 2);
+
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',0,'0:30','08:00','17:00', 3);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',1,'0:30','08:00','17:00', 3);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',2,'0:30','08:00','17:00', 3);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values ('15:30','15:00',3,'0:30','08:00','17:00', 3);
+insert into shiftplan_workdays (breaktime_end, breaktime_start, day_of_week, rest_time, worktime_entry, worktime_exit, shiftplan_id) values (null,null,4,'00:20','08:00','20:00', 3);
--- kayros-webapp/src/main/java/es/ozona/kayros/webapp/utils/ExportUtils.java
+++ kayros-webapp/src/main/java/es/ozona/kayros/webapp/utils/ExportUtils.java
@@ -2,6 +2,7 @@ package es.ozona.kayros.webapp.utils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -10,14 +11,14 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.QuoteMode;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.zkoss.util.resource.Labels;
 
@@ -45,8 +46,8 @@ public class ExportUtils {
 
 			StringWriter outstr = new StringWriter();
 
-			CSVPrinter printer = new CSVPrinter(outstr,
-					CSVFormat.DEFAULT.withQuote('"').withQuoteMode(QuoteMode.ALL).withDelimiter(';').withHeader(headers.toArray(new String[headers.size()])));
+			CSVPrinter printer = new CSVPrinter(outstr, CSVFormat.DEFAULT.withQuote('"').withQuoteMode(QuoteMode.ALL)
+					.withDelimiter(';').withHeader(headers.toArray(new String[headers.size()])));
 
 			for (ArrayList<Object> row : rows) {
 
@@ -72,7 +73,8 @@ public class ExportUtils {
 
 					} else if (array[x] instanceof LocalDate) {
 
-						array[x] = formater.format(Date.from(((LocalDate) array[x]).atStartOfDay(ZoneId.systemDefault()).toInstant()));
+						array[x] = formater.format(
+								Date.from(((LocalDate) array[x]).atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
 					}
 
@@ -100,94 +102,175 @@ public class ExportUtils {
 	}
 
 	public static InputStream exportXLSX(ArrayList<ArrayList<Object>> rows, ArrayList<String> headers) {
+		SimpleDateFormat fullFormater = new SimpleDateFormat(FULL_PATTERN);
+		SimpleDateFormat formater = new SimpleDateFormat(PATTERN);
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet(WORKING_TIMEPERIODS_TEXT);
 
-		Workbook workbook = new XSSFWorkbook();
+		int rowNum = 0;
+		Row headerRow = sheet.createRow(rowNum);
+		int colNum = 0;
+		for (String header : headers) {			
+			Cell cell = headerRow.createCell(colNum++);
+			cell.setCellValue(header);
+		}
 
-		try {
+		rowNum++;
 
-			SimpleDateFormat fullFormater = new SimpleDateFormat(FULL_PATTERN);
-			SimpleDateFormat formater = new SimpleDateFormat(PATTERN);
+		for (List<Object> rowData : rows) {
+			Row row = sheet.createRow(rowNum++);
+			colNum = 0;
+			for (Object data : rowData) {
+				Cell cell = row.createCell(colNum);
 
-			Sheet sheet = workbook.createSheet(WORKING_TIMEPERIODS_TEXT);
+				if (data instanceof Boolean) {
 
-			Row header = sheet.createRow(0);
-
-			for (int x = 0; x < headers.size(); x++) {
-
-				sheet.setColumnWidth(x, rows.size());
-				Cell cell = header.createCell(x);
-				cell.setCellValue(headers.get(x));
-
-			}
-
-			for (int y = 0; y < rows.size(); y++) {
-
-				Row row = sheet.createRow(y + 1);
-
-				for (int z = 0; z < rows.get(y).size(); z++) {
-
-					Cell cell = row.createCell(z);
-
-					if (rows.get(y).get(z) instanceof Boolean) {
-
-						if ((Boolean) rows.get(y).get(z) == true) {
-
-							cell.setCellValue(YES_TEST);
-
-						} else {
-
-							cell.setCellValue(NO_TEXT);
-
-						}
-
-					} else if (rows.get(y).get(z) instanceof Date) {
-
-						cell.setCellValue(fullFormater.format(rows.get(y).get(z)));
-
-					} else if (rows.get(y).get(z) instanceof LocalDate) {
-
-						cell.setCellValue(formater.format(Date.from(((LocalDate) rows.get(y).get(z)).atStartOfDay(ZoneId.systemDefault()).toInstant())));
-
+					if ((Boolean) data == true) {
+						cell.setCellValue(YES_TEST);
 					} else {
-
-						cell.setCellValue(rows.get(y).get(z) == null ? null : rows.get(y).get(z).toString());
-
+						cell.setCellValue(NO_TEXT);
 					}
-
+				} else if (data instanceof Date) {
+					cell.setCellValue(fullFormater.format(data));
+				} else if (data instanceof LocalDate) {
+					cell.setCellValue(formater
+							.format(Date.from(((LocalDate) data).atStartOfDay(ZoneId.systemDefault()).toInstant())));
+				} else {
+					cell.setCellValue(data != null? data.toString():"");
 				}
-
+				sheet.autoSizeColumn(colNum++);
 			}
+			
+		}
+
+		/*
+		 * Object[][] datatypes = { {"Datatype", "Type", "Size(in bytes)"}, {"int",
+		 * "Primitive", 2}, {"float", "Primitive", 4}, {"double", "Primitive", 8},
+		 * {"char", "Primitive", 1}, {"String", "Non-Primitive", "No fixed size"} };
+		 * 
+		 * int rowNum = 0; System.out.println("Creating excel");
+		 * 
+		 * for (Object[] datatype : datatypes) { Row row = sheet.createRow(rowNum++);
+		 * int colNum = 0; for (Object field : datatype) { Cell cell =
+		 * row.createCell(colNum++); if (field instanceof String) {
+		 * cell.setCellValue((String) field); } else if (field instanceof Integer) {
+		 * cell.setCellValue((Integer) field); } } }
+		 */
+		try
+
+		{
 
 			ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
 			workbook.write(buffer);
 			workbook.close();
 
 			byte[] bytes = buffer.toByteArray();
 			InputStream inputStream = new ByteArrayInputStream(bytes);
 
-			buffer.close();
-
 			return inputStream;
 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-
-			return null;
-
-		} finally {
-
-			try {
-
-				workbook.close();
-
-			} catch (IOException e) {
-
-				return null;
-
-			}
-
+			e.printStackTrace();
 		}
 
+		System.out.println("Done");
+
+		return null;
+
+		/*
+		 * Workbook workbook = new XSSFWorkbook();
+		 * 
+		 * try {
+		 * 
+		 * 
+		 * 
+		 * Sheet sheet = workbook.createSheet(WORKING_TIMEPERIODS_TEXT);
+		 * 
+		 * Row header = sheet.createRow(0);
+		 * 
+		 * for (int x = 0; x < headers.size(); x++) {
+		 * 
+		 * sheet.setColumnWidth(x, rows.size()); Cell cell = header.createCell(x);
+		 * cell.setCellValue(headers.get(x));
+		 * 
+		 * }
+		 * 
+		 * for (int y = 0; y < rows.size(); y++) {
+		 * 
+		 * Row row = sheet.createRow(y + 1);
+		 * 
+		 * for (int z = 0; z < rows.get(y).size(); z++) {
+		 * 
+		 * Cell cell = row.createCell(z);
+		 * 
+		 * if (rows.get(y).get(z) instanceof Boolean) {
+		 * 
+		 * if ((Boolean) rows.get(y).get(z) == true) {
+		 * 
+		 * cell.setCellValue(YES_TEST);
+		 * 
+		 * } else {
+		 * 
+		 * cell.setCellValue(NO_TEXT);
+		 * 
+		 * }
+		 * 
+		 * } else if (rows.get(y).get(z) instanceof Date) {
+		 * 
+		 * cell.setCellValue(fullFormater.format(rows.get(y).get(z)));
+		 * 
+		 * } else if (rows.get(y).get(z) instanceof LocalDate) {
+		 * 
+		 * cell.setCellValue(formater.format(Date.from(((LocalDate)
+		 * rows.get(y).get(z)).atStartOfDay(ZoneId.systemDefault()).toInstant())));
+		 * 
+		 * } else {
+		 * 
+		 * cell.setCellValue(rows.get(y).get(z) == null ? null :
+		 * rows.get(y).get(z).toString());
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		 * 
+		 * workbook.write(new FileOutputStream(new
+		 * File("c:\\proxectos\\registro.xls"))); workbook.close();
+		 * 
+		 * 
+		 * 
+		 * buffer.flush(); buffer.close();
+		 * 
+		 * byte[] bytes = buffer.toByteArray(); InputStream inputStream = new
+		 * ByteArrayInputStream(bytes);
+		 * 
+		 * buffer.close();
+		 * 
+		 * return inputStream;
+		 * 
+		 * } catch (IOException e) {
+		 * 
+		 * return null;
+		 * 
+		 * } finally {
+		 * 
+		 * try {
+		 * 
+		 * workbook.close();
+		 * 
+		 * } catch (IOException e) {
+		 * 
+		 * return null;
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
 	}
 
 }
